ef5c5f82f5eeaac54fc355e0f9e3ab02a7d01bdb
Anusha Mathew <67368087+MathewAnu@users.noreply.github.com>
2022-04-23 19:04:45 +0200

    MODIFIED:
    -Modiefied model Team, Employee
    
    -Created model TeamEmployee, TeamLeader

--- employee_management_system/admin.py
+++ employee_management_system/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Team)
-admin.site.register(Employee)
+admin.site.register(TeamLeader)
+admin.site.register(TeamEmployee)
--- employee_management_system/models.py
+++ employee_management_system/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Team(models.Model):
     team_name = models.CharField(max_length=20, primary_key=True)
-    team_leader = models.CharField(max_length=20, blank=True, default=None)
 
     def __str__(self):
         return self.team_name
@@ -11,8 +10,27 @@ class Team(models.Model):
 
 class Employee(models.Model):
     emp_name = models.CharField(max_length=50, default=None)
-    emp_team_name = models.ForeignKey(Team, on_delete=models.SET_NULL, default=None, null=True)
+    emp_team_name = models.ForeignKey(Team, on_delete=models.SET_NULL, default=None, null=True, related_name='%(class)s')
     emp_hourly_rate = models.FloatField(default=15)
 
     def __str__(self):
         return self.emp_name
+
+    class Meta:
+        abstract = True
+
+
+class TeamEmployee(Employee):
+    def __str__(self):
+        return self.emp_name
+
+
+class TeamLeader(Employee):
+    additional_pay_percentage = 10
+
+    def __str__(self):
+        return self.emp_name
+
+
+
+
