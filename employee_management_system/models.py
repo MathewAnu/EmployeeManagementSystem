ad166d8b6c3468723762ff96b08e3d39f48a8fee
Anusha Mathew <67368087+MathewAnu@users.noreply.github.com>
2022-04-24 01:10:49 +0200

    MODIFIED
    - Models modified
    
    - Created model WorkArrangements

--- employee_management_system/models.py
+++ employee_management_system/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+
+def work_arrangement_validator(values):
+    val_sum = 0
+    for value in values:
+        val_sum = val_sum+value
+    if val_sum > 100:
+        raise ValidationError("Work arrangement combination exceeds 100%")
 
 
 class Team(models.Model):
@@ -22,35 +31,31 @@ class WorkArrangement(models.Model):
 
 class Employee(models.Model):
     emp_name = models.CharField(max_length=50, default=None)
-    emp_team_name = models.ForeignKey(Team, on_delete=models.SET_NULL, default=None, null=True, related_name='%(class)s')
     emp_hourly_rate = models.FloatField(default=15)
+    #emp_work_arrangement = models.ManyToManyField(WorkArrangement, validators=[work_arrangement_validator])
     emp_work_arrangement = models.ManyToManyField(WorkArrangement)
 
     def __str__(self):
         return self.emp_name
 
-    class Meta:
-        abstract = True
 
-
-class TeamEmployee(Employee):
+class TeamEmployee(models.Model):
     additional_pay_percentage = 0
-    def __str__(self):
-        return self.emp_name
-
-
-class TeamLeader(Employee):
-    additional_pay_percentage = 10
-    #emp_name = models.ForeignKey(TeamEmployee, on_delete=models.CASCADE)
-    emp_team_name = models.OneToOneField(Team, on_delete=models.CASCADE)
+    emp_name = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='%(class)s', default=None, unique=True)
+    emp_team_name = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='%(class)s', default=None)
 
     def __str__(self):
         return str(self.emp_name)
 
-    def __del__(self):
-        pass
-    #destructor
-    #add team lead to TeamEmployee
+
+class TeamLeader(models.Model):
+    additional_pay_percentage = 10
+    emp_name = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='%(class)s', default=None)
+    emp_team_name = models.OneToOneField(Team, on_delete=models.CASCADE, related_name='%(class)s', default=None)
+
+    def __str__(self):
+        return str(self.emp_name)
+
 
 
 
