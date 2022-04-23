23a2d44082521e4a96d86e9fe395bd2beccce324
Anusha Mathew <67368087+MathewAnu@users.noreply.github.com>
2022-04-23 17:31:43 +0200

    MODIFIED:
    -Modiefied model Team
    
    Created model Employee

--- employee_management_system/models.py
+++ employee_management_system/models.py
@@ -4,4 +4,15 @@ from django.db import models
 class Team(models.Model):
     team_name = models.CharField(max_length=20, primary_key=True)
     team_leader = models.CharField(max_length=20, blank=True, default=None)
-    
+
+    def __str__(self):
+        return self.team_name
+
+
+class Employee(models.Model):
+    emp_name = models.CharField(max_length=50, default=None)
+    emp_team_name = models.ForeignKey(Team, on_delete=models.SET_NULL, default=None, null=True)
+    emp_hourly_rate = models.FloatField(default=15)
+
+    def __str__(self):
+        return self.emp_name
