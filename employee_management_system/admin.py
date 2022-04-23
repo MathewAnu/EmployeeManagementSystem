57e463e58d8373c3eb54ea45c20708cd84494e00
Anusha Mathew <67368087+MathewAnu@users.noreply.github.com>
2022-04-23 20:02:02 +0200

    MODIFIED:
    -Modiefied existing models
    
    -Created model WorkArrangement

--- employee_management_system/admin.py
+++ employee_management_system/admin.py
@@ -4,3 +4,4 @@ from .models import *
 admin.site.register(Team)
 admin.site.register(TeamLeader)
 admin.site.register(TeamEmployee)
+admin.site.register(WorkArrangement)
--- employee_management_system/models.py
+++ employee_management_system/models.py
@@ -8,10 +8,23 @@ class Team(models.Model):
         return self.team_name
 
 
+class WorkArrangement(models.Model):
+    work_percentage_options = ((100, 100), (75, 75), (50, 50), (25, 25))
+    work_time_percentange = models.IntegerField(choices=work_percentage_options, default=100)
+    if work_time_percentange == 100:
+        work_type = models.CharField(max_length=15, editable=False, default="Full-time")
+    else:
+        work_type = models.CharField(max_length=15, editable=False, default="Part-time")
+
+    def __str__(self):
+        return str(self.work_time_percentange)
+
+
 class Employee(models.Model):
     emp_name = models.CharField(max_length=50, default=None)
     emp_team_name = models.ForeignKey(Team, on_delete=models.SET_NULL, default=None, null=True, related_name='%(class)s')
     emp_hourly_rate = models.FloatField(default=15)
+    emp_work_arrangement = models.ManyToManyField(WorkArrangement)
 
     def __str__(self):
         return self.emp_name
@@ -21,15 +34,25 @@ class Employee(models.Model):
 
 
 class TeamEmployee(Employee):
+    additional_pay_percentage = 0
     def __str__(self):
         return self.emp_name
 
 
 class TeamLeader(Employee):
     additional_pay_percentage = 10
+    #emp_name = models.ForeignKey(TeamEmployee, on_delete=models.CASCADE)
+    emp_team_name = models.OneToOneField(Team, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.emp_name
+        return str(self.emp_name)
+
+    def __del__(self):
+        pass
+    #destructor
+    #add team lead to TeamEmployee
+
+
 
 
 
